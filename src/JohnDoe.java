76e9de87197cffd77733bb1f752e4d59ea6b684b
Dannysib <daniel.sibrian@csuglobal.edu>
2024-01-15 21:34:14 -0700

    Added code for Assignment

---
+++ src/JohnDoe.java
@@ -0,0 +1,9 @@
+public class JohnDoe {
+    public static void main (String [] args) {
+        System.out.println("John");
+        System.out.println("Doe");
+        System.out.println("1231 S Street View");
+        System.out.println("Townsland");
+        System.out.println("87121");
+    }
+}
